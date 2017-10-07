3604d82ec76c127b4a69e5b90521b521114c58bc
Shantanu Narale <shantanunarale061@gmail.com>
2017-10-07 13:10:52 +0530

    Added Create DB script

---
+++ ProjectXDB/ProjectXDB/Scripts/Not In Build/CreateDBScript.sql
@@ -0,0 +1,16 @@
+﻿/*Create Database Script*/
+CREATE DATABASE ProjectX
+ON
+(
+	NAME = ProjectX_dat,
+	FILENAME = 'F:\SQLProj\ProjectX\Data\ProjectX.mdf',
+	SIZE = 10MB,
+	FILEGROWTH = 1MB
+)
+LOG ON
+(
+	NAME = ProjectX_log,
+	FILENAME = 'F:\SQLProj\ProjectX\Logs\ProjectX.ldf',
+	SIZE = 512KB,
+	FILEGROWTH = 100KB
+)
